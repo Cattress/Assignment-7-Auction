433066e6a0b8b7d618275e1a6822da9e6ee00a01
Cattress <priyaphon.vincent@gmail.com>
2021-01-26 13:46:49 +0700

    Add project files.

---
+++ Assignment 7 Auction/Items.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_7_Auction
+{
+    class Items
+    {
+        private string ItemDesc;
+        private double RsrvPrice;
+        private double HighestBid;
+        private int BidderID;
+    }
+
+    public SetDesc(string ItemDesc)
+    {
+        this.ItemDesc = ItemDesc;
+    }
+
+    public SetRsrvP(double RsrvPrice)
+    {
+        this.RsrvPrice = RsrvPrice;
+    }
+   
+    public double GetHbid()
+    {
+        return HighestBid;
+    }
+
+}
---
+++ Assignment 7 Auction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_7_Auction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            items[0] = new Items("Golden Cat", 1000, );
+        }
+    }
+}
